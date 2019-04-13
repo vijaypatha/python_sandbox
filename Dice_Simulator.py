00da9e94d33a7ecd904d45417e5ee027ce535318
Patha <vipatha@amazon.com>
2019-04-13 16:53:13 -0700

    dice

---
+++ Dice_Simulator.py
@@ -0,0 +1,17 @@
+''' 
+Rolling a Dice Simulator
+
+The Goal: Like the title suggests, this project involves writing a program that simulates rolling dice. When the program runs, it will randomly choose a number between 1 and 6. (Or whatever other integer you prefer — the number of sides on the die is up to you.) The program will print what that number is. It should then ask you if you’d like to roll again. For this project, you’ll need to set the min and max number that your dice can produce. For the average die, that means a minimum of 1 and a maximum of 6. You’ll also want a function that randomly grabs a number within that range and prints it.
+'''
+
+#psudocode
+'''
+roll once -> randomly choose between 1 and 6
+print("random number")
+should I roll again? Y/N? 
+Y
+roll -> randomly choose between 1 and 6
+print("random number")
+'''
+
+
