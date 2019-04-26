93c1639587a0d15f0a5eab9028bd32037eb0ad92
Patha <vipatha@amazon.com>
2019-04-25 21:19:12 -0700

    ndarrray

--- numpy.py
+++ numpy.py
@@ -7,6 +7,12 @@ Why numpy?
 Even though Python lists are great on their own, NumPy has a number of key features that give it great advantages over Python lists.
 1) Speed
 2) multidimensional array data structures that can represent vectors and matrices.(a lot of machine learning algorithms rely on matrix operations.)
+3) Another great advantage of NumPy over Python lists is that NumPy has a large number of optimized built-in mathematical functions.
 
+What is multidimensional array data structure? 
+At the core of NumPy is the ndarray, where nd stands for n-dimensional. An ndarray is a multidimensional array of elements all of the same type. 
+
+What is ndarray? 
+In other words, an ndarray is a grid that can take on many shapes and can hold either numbers or strings. In many Machine Learning problems you will often find yourself using ndarrays in many different ways. For instance, you might use an ndarray to hold the pixel values of an image that will be fed into a Neural Network for image classification.
 
 '''
