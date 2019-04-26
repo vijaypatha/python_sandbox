9c3f7ac5e4bfc193d0b70c69959f8d77265e2971
Patha <vipatha@amazon.com>
2019-04-25 21:16:02 -0700

    numpy

---
+++ numpy.py
@@ -0,0 +1,12 @@
+'''
+What is numpy?
+
+NumPy stands for Numerical Python and it's a fundamental package for scientific computing in Python. NumPy provides Python with an extensive math library capable of performing numerical computations effectively and efficiently. 
+
+Why numpy?
+Even though Python lists are great on their own, NumPy has a number of key features that give it great advantages over Python lists.
+1) Speed
+2) multidimensional array data structures that can represent vectors and matrices.(a lot of machine learning algorithms rely on matrix operations.)
+
+
+'''
