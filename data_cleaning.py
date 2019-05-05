d67a02955b6b572e11231e4ad97c0001358f347a
Patha <vipatha@amazon.com>
2019-05-04 19:55:45 -0700

    regex

--- data_cleaning.py
+++ data_cleaning.py
@@ -30,4 +30,31 @@ although many indexing and merging operations will notice a speedup in runtime i
 '''
 df.get_dtype_counts()
 df.dtypes
+#at index 1905 and onwards, give me the values of the column Date of Publication. 
 df.loc[1905:, 'Date of Publication'].head(10)
+'''
+A particular book can have only one date of publication. Therefore, we need to do the following:
+
+Remove the extra dates in square brackets, wherever present: 1879 [1878]
+Convert date ranges to their “start date”, wherever present: 1860-63; 1839, 38-54
+Completely remove the dates we are not certain about and replace them with NumPy’s NaN: [1897?]
+Convert the string nan to NumPy’s NaN value
+'''
+
+'''
+we can actually take advantage of a single regular expression to extract the publication year:
+
+regex = r'^(\d{4})'
+
+The regular expression above is meant to find any four digits at the beginning of a string, which suffices for our case. 
+The above is a raw string (meaning that a backslash is no longer an escape character), which is standard practice with regular expressions.
+
+The \d represents any digit, and {4} repeats this rule four times. 
+The ^ character matches the start of a string, and the parentheses denote a capturing group, which signals to Pandas that we want to extract that part of the regex. 
+(We want ^ to avoid cases where [ starts off the string.)
+'''
+extr = df['Date of Publication'].str.extract(r'^(\d{4})', expand=False)
+extr.loc[1905:,].head(20)
+extr.head(10)
+
+df['Date of Publication'] = pd.to_numeric(extr)
